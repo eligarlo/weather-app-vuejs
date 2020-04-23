2d7567fdeae300060f1668d43cedfd251da07ca1
eligarlo <eligarlo.developer@gmail.com>
2020-04-23 10:09:54 +0300

    calling the weather component from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,16 @@
 <template>
 	<div id="app">
-		<main>
-			<div class="search-box">
-				<input type="text" class="search-bar" placeholder="Search...">
-			</div>
-
-			<div class="weather-wrap">
-				<div class="location-box">
-					<div class="location">
-						Madrid, Spain
-					</div>
-					<div class="date">
-						Monday 20 January 2020
-					</div>
-				</div>
-
-				<div class="weather-box">
-					<div class="temperature">
-						9°c
-					</div>
-					<div class="weather">
-						Rain
-					</div>
-				</div>
-			</div>
-		</main>
+		<Weather/>
 	</div>
 </template>
 
 <script>
+import Weather from './components/Weather';
+
 export default {
 	name: "App",
-	data() {
-		return {
-			api_key: 'd83845dda52973af0dc6d7e57c8f6c26'
-		}
+	components: {
+		Weather
 	}
 };
 </script>
@@ -48,87 +24,5 @@ export default {
 
 body {
 	font-family: 'montserrat', sans-serif;
-}
-
-#app {
-	/* background: url('./assets/cold-bg.jpg') bottom cover; */
-	background-image: url('./assets/cold-bg.jpg');
-	background-size: cover;
-	background-position: bottom;
-	transition: 0.4s;
-}
-
-main {
-	min-height: 100vh;
-	padding: 25px;
-	background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
-}
-
-.search-box {
-	width: 100%;
-	margin-bottom: 30px;
-}
-
-.search-box .search-bar {
-	display: block;
-	width: 100%;
-	padding: 15px;
-	color: #313131;
-	font-size: 20px;
-	appearance: none;
-	border: none;
-	outline: none;
-	background: none;
-	box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-	background-color: rgba(255, 255, 255, 0.5);
-	border-radius: 0 16px;
-	transition: 0.4s;
-}
-
-.search-box .search-bar:focus {
-	box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-	background-color: rgba(255, 255, 255, 0.75);
-	border-radius: 16px 0;
-}
-
-.location-box .location {
-	color: #FFF;
-	font-size: 32px;
-	font-weight: 500;
-	text-align: center;
-	text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-}
-
-.location-box .date {
-	color: #FFF;
-	font-size: 20px;
-	font-weight: 300;
-	font-style: italic;
-	text-align: center;
-}
-
-.weather-box {
-	text-align: center;
-}
-
-.weather-box .temperature {
-	display: inline-block;
-	padding: 10px 25px;
-	color: #FFF;
-	font-size: 102px;
-	font-weight: 900;
-	text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-	background-color: rgba(255, 255, 255, 0.25);
-	border-radius: 16px;
-	margin: 30px 0;
-	box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-}
-
-.weather-box .weather {
-	color: #FFF;
-	font-size: 48px;
-	font-weight: 700;
-	font-style: italic;
-	text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
