5b89c287314c5c68ad5c7de4737c349d79553233
eligarlo <eligarlo.developer@gmail.com>
2020-07-22 20:40:40 +0300

    environment variable added

--- src/services/Weather-Api-Service.js
+++ src/services/Weather-Api-Service.js
@@ -1,6 +1,6 @@
 class WeatherAPIService {
     async fetchWeather(url, apiKey, query) {
-        let response = await fetch(`${url}weather?q=${query}&units=metric&APPID=${apiKey}`);
+        let response = await fetch(`${url}weather?q=${query}&units=metric&APPID=${process.env.VUE_APP_WEATHER_API_KEY}`);
         let json = await response.json();
         return await json;
     }
